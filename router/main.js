47ddfd85ad70564159e91cdbebf430ee3f9aa0c0
yoonajoo <judy0_1@naver.com>
2021-03-17 18:03:08 +0900

    2

--- router/main.js
+++ router/main.js
@@ -11,8 +11,25 @@ module.exports = function(app) {
      });
      app.get('/about',function(req,res){
         res.render('about.html');
-    });
+      });
+      app.get('/blog-detail',function(req,res){
+         res.render('blog-detail.html');
+      });
+      app.get('/blog',function(req,res){
+         res.render('blog.html');
+      });
+      app.get('/contact',function(req,res){
+            res.render('contact.html');
+      });
+      app.get('/project-detail',function(req,res){
+         res.render('project-detail.html');
+      });
      
+      // 여기까지 views 에 있는것들 app.get 불러오기 만들기 
+
+
+
+
     app.get('/listall', function (req, res) {
       //res.render('about.html');
       console.log('listall...');
